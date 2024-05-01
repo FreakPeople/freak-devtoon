de015872853bdcf88a0d24f1db4d049ba6dd9047
jeongjihun <wlgns2234@naver.com>
2024-05-01 16:09:29 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/yjh/devtoon/DevtoonApplication.java
@@ -0,0 +1,13 @@
+package yjh.devtoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yjh/devtoon/DevtoonApplicationTests.java
@@ -0,0 +1,13 @@
+package yjh.devtoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
