cca5619a8709ab82acca3326e75b6ca2808ce87b
Lea Hwang <dbwjd5032@gmail.com>
2024-05-08 02:14:20 +0900

    feat: 활성 정책 조회 기능 구현
    - 현재 시간 기준으로 활성화된 정책들을 조회합니다.
    - 응답 DTO 내 정적 내부 클래스 `PolicyDetailsInfo`를 사용하여 정책 관련 세부 정보를 구현했습니다.

--- src/main/java/yjh/devtoon/policy/presentation/PoilcyController.java
+++ src/main/java/yjh/devtoon/policy/presentation/PoilcyController.java
@@ -3,6 +3,7 @@ package yjh.devtoon.policy.presentation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import yjh.devtoon.common.response.Response;
 import yjh.devtoon.policy.application.PolicyService;
 import yjh.devtoon.policy.dto.request.PolicyCreateRequest;
+import yjh.devtoon.policy.dto.response.RetrieveActivePoliciesResponse;
 
 @Slf4j
 @RequestMapping("/v1/policies")
@@ -31,6 +33,18 @@ public class PoilcyController {
         log.info("새로운 정책 등록: ID={}", policyId);
 
         return ResponseEntity.ok(Response.success(policyId));
+    }
+
+    /**
+     * 활성 정책 조회
+     * : 현재 시간 기준으로 활성화된 정책들을 조회합니다.
+     */
+    @GetMapping("/active")
+    public ResponseEntity<Response<RetrieveActivePoliciesResponse>> getActivePolicies() {
+        RetrieveActivePoliciesResponse response = policyService.getActivePolicies();
+        log.info("활성 정책 조회: {}", response);
+
+        return ResponseEntity.ok(Response.success(response));
     }
 
 }
