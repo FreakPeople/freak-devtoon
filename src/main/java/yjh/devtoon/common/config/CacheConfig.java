826d8e70d8ce793ae698aa996ce060b56dd2f8d5
Lea Hwang <dbwjd5032@gmail.com>
2024-05-28 20:55:55 +0900

    feat: Caffeine 기반 캐시 설정 추가
    - Caffeine을 이용한 캐시 설정을 위해 CacheConfig 파일 추가
    - CacheType 열거형을 사용하여 캐시의 기본 TTL 및 최대 크기 설정

--- src/main/java/yjh/devtoon/common/config/CacheConfig.java
+++ src/main/java/yjh/devtoon/common/config/CacheConfig.java
@@ -1,13 +1,14 @@
 package yjh.devtoon.common.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.Expiry;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import yjh.devtoon.promotion.domain.promotion.CookiePromotion;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @EnableCaching
@@ -15,36 +16,24 @@ import java.util.concurrent.TimeUnit;
 public class CacheConfig {
 
     @Bean
-    public Caffeine<Long, CookiePromotion> caffeineConfig() {
-        return Caffeine.newBuilder()
-                .expireAfter(new Expiry<Long, CookiePromotion>() {
-                    @Override
-                    public long expireAfterCreate(Long key, CookiePromotion value,
-                                                  long currentTime) {
-                        // 각 항목에 대해 개별적으로 만료 시간 지정 (예: 60분)
-                        return TimeUnit.MINUTES.toNanos(60);
-                    }
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
 
-                    @Override
-                    public long expireAfterUpdate(Long key, CookiePromotion value,
-                                                  long currentTime, long currentDuration) {
-                        // 업데이트 시 만료 시간 지정 (필요에 따라)
-                        return currentDuration;
-                    }
-
-                    @Override
-                    public long expireAfterRead(Long key, CookiePromotion value, long currentTime
-                            , long currentDuration) {
-                        // 읽기 시 만료 시간 지정 (필요에 따라)
-                        return currentDuration;
-                    }
-                });
+        List<CaffeineCache> caches = Arrays.stream(CacheType.values())
+                .map(this::buildCache)
+                .toList();
+        cacheManager.setCaches(caches);
+        return cacheManager;
     }
 
-    @Bean
-    public CacheManager cacheManager(Caffeine caffeine) {
-        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-        caffeineCacheManager.setCaffeine(caffeine);
-        return caffeineCacheManager;
+    private CaffeineCache buildCache(CacheType cacheType) {
+        return new CaffeineCache(
+                cacheType.getName(),
+                Caffeine.newBuilder()
+                        .expireAfterWrite(cacheType.getExpireAfterWrite(), TimeUnit.SECONDS)
+                        .maximumSize(cacheType.getMaximumSize())
+                        .build()
+        );
     }
+
 }
