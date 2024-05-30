ca0cab682b66acedd3e57b9051a22b16738573e6
Lea Hwang <dbwjd5032@gmail.com>
2024-05-30 20:29:36 +0900

    refactor: CacheConfig 파일 수정
    - CaffeineCacheManager를 사용하여 캐시 매니저 구성
    - 필요한 부분만 적음
    - 캐시 만료 시간, 최대 캐시 사이즈 해당 파일에서 설정

--- src/main/java/yjh/devtoon/common/config/CacheConfig.java
+++ src/main/java/yjh/devtoon/common/config/CacheConfig.java
@@ -3,12 +3,9 @@ package yjh.devtoon.common.config;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @EnableCaching
@@ -17,23 +14,11 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-
-        List<CaffeineCache> caches = Arrays.stream(CacheType.values())
-                .map(this::buildCache)
-                .toList();
-        cacheManager.setCaches(caches);
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("promotion");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(120, TimeUnit.SECONDS)
+                .maximumSize(100));
         return cacheManager;
-    }
-
-    private CaffeineCache buildCache(CacheType cacheType) {
-        return new CaffeineCache(
-                cacheType.getName(),
-                Caffeine.newBuilder()
-                        .expireAfterWrite(cacheType.getExpireAfterWrite(), TimeUnit.SECONDS)
-                        .maximumSize(cacheType.getMaximumSize())
-                        .build()
-        );
     }
 
 }
