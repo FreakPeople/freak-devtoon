cf46d2a280f6f5c31c9e28824b5185b95ee48361
Lea Hwang <dbwjd5032@gmail.com>
2024-05-31 13:58:31 +0900

    test: 캐시 적용 프로모션 등록 테스트 수정
    - 프로모션 저장 후 캐시 업데이트 메서드가 정확히 호출되는지 검증
    - ArgumentCaptor를 사용하여 updatePromotionInCache 메서드 호출 시 전달된 PromotionEntity 객체를 캡처

--- src/test/java/yjh/devtoon/promotion/application/PromotionServiceTest.java
+++ src/test/java/yjh/devtoon/promotion/application/PromotionServiceTest.java
@@ -1,15 +1,22 @@
 package yjh.devtoon.promotion.application;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import yjh.devtoon.promotion.domain.DiscountType;
+import yjh.devtoon.promotion.domain.PromotionEntity;
 import yjh.devtoon.promotion.dto.request.PromotionAttributeCreateRequest;
 import yjh.devtoon.promotion.dto.request.PromotionCreateRequest;
 import yjh.devtoon.promotion.infrastructure.PromotionAttributeRepository;
@@ -30,6 +37,12 @@ class PromotionServiceTest {
     @Mock
     private PromotionAttributeRepository promotionAttributeRepository;
 
+    @Mock
+    private PromotionCacheService promotionCacheService;
+
+    @Captor
+    private ArgumentCaptor<PromotionEntity> promotionEntityCaptor;
+
     @Nested
     @DisplayName("프로모션 등록 테스트")
     class RegisterPromotionTests {
@@ -38,7 +51,7 @@ class PromotionServiceTest {
         private static final BigDecimal DISCOUNT_RATE = null;
         private static final Integer DISCOUNT_QUANTITY = 1;
         private static final Boolean IS_DISCOUNT_DUPLICATABLE = true;
-        private static final LocalDateTime STARTDATE = LocalDateTime.of(2024, 7, 15, 0, 0, 0);
+        private static final LocalDateTime STARTDATE = LocalDateTime.of(2024, 5, 13, 13, 50, 0);
         private static final LocalDateTime ENDDATE = LocalDateTime.of(2024, 8, 15, 11, 59, 59);
         private static final List<PromotionAttributeCreateRequest> PROMOTION_ATTRIBUTES = List.of(
                 new PromotionAttributeCreateRequest("target-month", "7"),
@@ -60,11 +73,28 @@ class PromotionServiceTest {
                     PROMOTION_ATTRIBUTES
             );
 
-            // when, then
+            // mocking
+            when(promotionRepository.save(any(PromotionEntity.class))).thenAnswer(invocation -> {
+                PromotionEntity promotion = invocation.getArgument(0);
+                return PromotionEntity.builder()
+                        .id(1L)
+                        .description(promotion.getDescription())
+                        .discountType(promotion.getDiscountType())
+                        .discountRate(promotion.getDiscountRate())
+                        .discountQuantity(promotion.getDiscountQuantity())
+                        .isDiscountDuplicatable(promotion.getIsDiscountDuplicatable())
+                        .startDate(promotion.getStartDate())
+                        .endDate(promotion.getEndDate())
+                        .build();
+            });
+
+            // when
             assertThatCode(() -> promotionService.register(request))
                     .doesNotThrowAnyException();
+
+            // then
+            verify(promotionRepository, times(1)).save(any(PromotionEntity.class));
+            verify(promotionCacheService, times(1)).updatePromotionInCache(promotionEntityCaptor.capture());
         }
-
     }
-
 }
